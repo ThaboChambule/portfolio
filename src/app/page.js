1f8fdc0fc70237641641a2030818b7a5f0adb286
Thabo <thabochambule1@gmail.com>
2024-11-02 04:14:59 +0200

    adjust display positioning

--- src/app/page.js
+++ src/app/page.js
@@ -5,24 +5,25 @@ import ParticlesComponent from "./components/Particles";
 export default function Home() {
   return (
     <>
-    <div className="relative">
-      <main>
-        <ParticlesComponent className="absolute inset-0 z-0" />
-        <div className="relative z-10">
-        <Header />
-          <div className="justify-self-center text-5xl py-80">
-            <h1>
-              Hello, I'm <span className="text-violet-500">Thabo</span>.
-            </h1>
-            <h1>I'm a full stack web developer</h1>
-            <div>
-              <Link href="/">View my work</Link>
+      <div className="relative">
+        <main>
+          <ParticlesComponent className="absolute inset-0 z-0" />
+          <div className="relative z-10">
+            <Header />
+            <div className="justify-self-center text-5xl py-80">
+              <h1>
+                Hello, I'm <span className="text-violet-500">Thabo</span>.
+              </h1>
+              <h1>I'm a full stack web developer</h1>
+              <div>
+                <Link href="/">View my work</Link>
+              </div>
             </div>
           </div>
-        </div>
-      </main>
-    </div>
-    <h1>Haibpewifopn</h1>
+        </main>
+      </div>
+      <h1 className="mt-4 relative bg-black">This is a test</h1>
+    
     </>
   );
 }
