193d59c5a09ce8c5b058116a445fdceba31bb6f6
Thabo <thabochambule1@gmail.com>
2024-11-02 09:46:01 +0200

    add form

--- src/app/pages/contact.js
+++ src/app/pages/contact.js
@@ -4,10 +4,12 @@ export default function Contact() {
       <h1 className="text-8xl text-center">Contact</h1>
       <h2 className="text-center">Have a question or want us to work together? Leave your<br/>
       details and I'll get back to you as soon as possible.</h2>
-
-
-
-
+    <form className="text-center">
+        <input placeholder="Name"/> <br/>
+        <input placeholder="Email"/> <br/>
+        <textarea placeholder="Message"/>
+        <button>Sumit</button>
+    </form>
 
     </div>
    
