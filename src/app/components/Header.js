2e9335a867a4ef817e4c5a8598cfd18e079c24dd
Thabo <thabochambule1@gmail.com>
2024-12-05 22:41:03 +0200

    adjust header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,27 +1,29 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Header() {
-    return (
-      <header className="flex justify-between">
-        <div className="text-5xl font-semibold ml-10 mt-5">
-          <Link  href="/">Thabo<span className="text-violet-500">.</span></Link>
+  return (
+    <header className=" text-white p-6">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-5xl font-semibold">
+          <Link href="/">
+            Thabo<span className="text-violet-500">.</span>
+          </Link>
         </div>
-        <div>
-        <nav className=" flex gap-5 mr-10 mt-11 text-2xl">
-            <div>
-            <Link href="/">Home</Link>
-            </div>
-            <div>
-            <Link href="#about">About</Link>
-            </div>
-            <div>
-            <Link href="#projects">Projects</Link>
-            </div>
-            <div>
-            <Link href="#contact">Contact</Link>
-            </div>
+        <nav className="flex gap-5 text-2xl">
+          <Link href="/" className="hover:text-violet-500">
+            Home
+          </Link>
+          <Link href="#about" className="hover:text-violet-500">
+            About
+          </Link>
+          <Link href="#projects" className="hover:text-violet-500">
+            Projects
+          </Link>
+          <Link href="#contact" className="hover:text-violet-500">
+            Contact
+          </Link>
         </nav>
-        </div>
-      </header>
-    );
-  }
+      </div>
+    </header>
+  );
+}
